fc086a2055cfbee65b821cc3945129e39619f36d
sowji <showshimya@gmail.com>
2015-09-15 15:21:06 -0400

    Removed comments added previously

--- polls/views.py
+++ polls/views.py
@@ -7,8 +7,7 @@ from django.utils import timezone
 
 from .models import Question, Choice
 # Create your views here.
-# making change to push changes
-# Adding comment to send to server so that clone can pull it back
+
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
     context_object_name = 'latest_question_list'
